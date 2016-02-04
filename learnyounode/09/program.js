e8dd3b3bfab44189214d909005705166fd355733
Robert <robert@flatduckrecords.co.uk>
2016-02-04 03:02:15 +0000

    Juggling Async

--- learnyounode/09/program.js
+++ learnyounode/09/program.js
@@ -1,29 +1,24 @@
 var	mymodule = require('./mymodule.js'),
     urls = process.argv.slice(2),
-    count = urls.length,
     done = 0,
     output = [];
-    
-
-for(var i = 0; i < count; i++){
-
-mymodule(process.argv[i+2], i, function (err, data) {
-	if(err) {
-		console.log('There was an error.');
-	} else {
-		output[data.index] = data.data.toString();
-		done++;
-		final();
-	}
-});
 
 
-//console.log(output.length);
-	
+for(var i = 0; i < urls.length; i++){
+
+	mymodule(process.argv[i+2], i, function (err, data) {
+		if(err) {
+			console.log('There was an error.');
+		} else {
+			output[data.index] = data.data.toString();
+			done++;
+			final();
+		}
+	});	
 }
 
 function final() {
-	if(done === count){
+	if(done === urls.length){
 		console.log(output.join('\n'));	
 	}
 }
